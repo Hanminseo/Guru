9014b4a5637a089b7a337178784ab81c22d0c64e
hhaann00 <hhj2983@gmail.com>
2021-02-01 12:34:26 +0900

    하단 네비게이션 바, 원형 프로그레스 바 설정

---
+++ app/src/main/java/com/example/swu_2/Frag01.kt
@@ -0,0 +1,19 @@
+package com.example.swu_2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Frag01 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frag01, container, false)
+    }
+
+}
--- app/src/main/java/com/example/swu_2/MainActivity.kt
+++ app/src/main/java/com/example/swu_2/MainActivity.kt
@@ -2,17 +2,26 @@ package com.example.swu_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.CalendarView
 import android.widget.ImageButton
 import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener,
+    CircleProgressBar.ProgressFormatter {
 
+    //변수
     lateinit var userID : TextView
     lateinit var userIDcheck : TextView
     lateinit var userPage : ImageButton
     lateinit var mainHome : ImageButton
     lateinit var myCalendar : CalendarView
+
+    // 원형 프로그레스 바 설정 변수
+    private val DEFAULT_PATTERN = "%d%%"
+    lateinit var circleProgressBar: CircleProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +31,41 @@ class MainActivity : AppCompatActivity() {
         userIDcheck = findViewById(R.id.userIDcheck)
         userPage = findViewById(R.id.userPage)
         mainHome = findViewById(R.id.mainHome)
-        myCalendar = findViewById(R.id.myCalendar)    }
+        myCalendar = findViewById(R.id.myCalendar)
+
+        val bottomNavigationView = findViewById<View>(R.id.bottom_navigation) as BottomNavigationView
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+        circleProgressBar=findViewById(R.id.cpb_circlebar);
+        circleProgressBar.setProgress(70);  // 해당 퍼센트를 적용
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_search -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frame_container, Frag01())
+                    .commit()
+                return true
+            }
+            R.id.nav_settings -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frame_container, Frag01())
+                    .commit()
+                return true
+            }
+            R.id.nav_navigation -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frame_container, Frag01())
+                    .commit()
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun format(progress: Int, max: Int): CharSequence? {
+        return String.format(
+            DEFAULT_PATTERN,
+            (progress.toFloat() / max.toFloat() * 100).toInt()
+        )
+    }
 }
