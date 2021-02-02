6be5e8494061d7ee1d2956a49b7a126666d54c41
Leesojeong7 <star6845@naver.com>
2021-02-02 11:01:23 +0900

    상단바 변경

--- app/src/main/java/com/example/swu_2/MainActivity.kt
+++ app/src/main/java/com/example/swu_2/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.swu_2
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.view.WindowManager
 import android.widget.CalendarView
 import android.widget.ImageButton
 import android.widget.TextView
