76ca373a59ed328fbf7086f063331cb2fc8d6ca3
Hanminseo <hm5260@naver.com>
2021-02-04 17:43:59 +0900

    미리 올려보는 새로만든 파일덜

---
+++ app/src/main/java/com/example/swu_2/SwuInfo.kt
@@ -0,0 +1,12 @@
+package com.example.swu_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SwuInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.swu_info)
+    }
+}
