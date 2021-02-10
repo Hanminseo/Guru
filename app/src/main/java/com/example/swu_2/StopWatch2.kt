31cb8b3369f9780e1ea6cbf478020cfdbbe3fc9c
손현지 <gus9637@naver.com>
2021-02-10 23:09:03 +0900

    xml 배경색

--- app/src/main/java/com/example/swu_2/StopWatch2.kt
+++ app/src/main/java/com/example/swu_2/StopWatch2.kt
@@ -108,7 +108,9 @@ class StopWatch2 : Fragment() {
 
     //시작 버튼 눌렀을때
     private fun start() {
-        time = 59000
+
+        time = 390000
+
         if (subjectName.text.toString() == "") {
             Toast.makeText(context?.applicationContext, "과목명을 입력해주세요", Toast.LENGTH_SHORT).show()
             return
@@ -118,7 +120,7 @@ class StopWatch2 : Fragment() {
         timerTask = timer(period = 10) {
             time++
 
-            hour = (time / 360000) % 24
+            hour = (time / 360000)%24
             min = time / 6000 % 60
             sec = time / 100 % 60
             watch = String.format("%02d : %02d : %02d", hour, min, sec)
